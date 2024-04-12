8a95badcba66600f120fdf6a1f5ec14d507c36e7
quielLovesLasagna <exequielarco23@gmail.com>
2024-04-12 10:18:27 +0800

    Initial step of using params for state management

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -38,16 +39,19 @@ function App() {
 				<Route index element={<Homepage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
-				<Route path="/login" element={<Login />} />
+				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
 					<Route
 						index
 						element={<CityList cities={cities} isLoading={isLoading} />}
 					/>
+
 					<Route
 						path="cities"
 						element={<CityList cities={cities} isLoading={isLoading} />}
 					/>
+					<Route path="cities/:id" element={<City />} />
+
 					<Route
 						path="countries"
 						element={<CountryList cities={cities} isLoading={isLoading} />}
