285d5ccdfebb59156e292c8d3efee55fa7db12ec
quielLovesLasagna <exequielarco23@gmail.com>
2024-04-16 09:40:50 +0800

    Added notes for programmatic navigation with useNavigate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
+	const navigate = useNavigate();
+
 	// This is for QUERY STRINGS
 	// This is like useState()
 	const [searchParams, setSearchParams] = useSearchParams();
@@ -9,7 +11,7 @@ function Map() {
 	const lng = searchParams.get("lng");
 
 	return (
-		<div className={styles.mapContainer}>
+		<div className={styles.mapContainer} onClick={() => navigate("form")}>
 			<h1>Map</h1>
 			<h2>
 				Position: {lat}, {lng}
