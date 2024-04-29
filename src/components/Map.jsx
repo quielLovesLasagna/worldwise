f20b0926e6a96255ceece74394f6430f55379b26
quielLovesLasagna <exequielarco23@gmail.com>
2024-04-29 21:59:22 +0800

    Setting map location with Geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,15 +10,19 @@ import {
 } from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
 	const { cities } = useCities();
-
 	const [mapPosition, setMapPosition] = useState([40, 0]);
-
-	// This is for QUERY STRINGS
-	// This is like useState()
 	const [searchParams] = useSearchParams();
+	const {
+		isLoading: isLoadingPosition,
+		position: geolocationPosition,
+		getPosition,
+	} = useGeolocation();
+
 	const mapLat = searchParams.get("lat");
 	const mapLng = searchParams.get("lng");
 
@@ -31,8 +35,22 @@ function Map() {
 		[mapLat, mapLng]
 	);
 
+	useEffect(
+		function () {
+			if (geolocationPosition) {
+				setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+			}
+		},
+		[geolocationPosition]
+	);
+
 	return (
 		<div className={styles.mapContainer}>
+			{!geolocationPosition && (
+				<Button type="position" onClick={getPosition}>
+					{isLoadingPosition ? "Loading..." : "Use your position"}
+				</Button>
+			)}
 			<MapContainer
 				center={mapPosition}
 				zoom={6}
